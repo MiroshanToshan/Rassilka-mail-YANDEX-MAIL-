39e43cc0a4bc066458ae24c7b392279c3fdf8c67
MiroshanToshan <abakumovmiron608@gmail.com>
2024-10-22 19:01:05 +0500

    New changes: - Added a settings window. -Fixed bugs. -Simplified functionality. Enjoy!

---
+++ defsender.py
@@ -0,0 +1,122 @@
+import os
+from dotenv import load_dotenv
+from send import send_mail
+import json 
+import argparse
+import customtkinter
+from settings import config
+load_dotenv()
+
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+okno = customtkinter.CTk() 
+okno.geometry("400x240")
+
+customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("dark")
+
+
+
+def start():
+  with open("mails.json", "r", encoding="UTF-8") as mails_js:
+    mails = mails_js.read()
+
+  with open("text.txt", "r", encoding="UTF-8") as text_txt:
+    text = text_txt.read()
+  mails = json.loads(mails)
+
+  with open("config.json", "r", encoding="UTF-8") as config_js:
+    configs = config_js.read()
+  configs = json.loads(configs)
+
+  text = text.replace("%website%", "https://boulderbugle.com/-133m2lwo")
+  text= text.replace("%friend_name%", friend_name.get())
+  text = text.replace("%my_name%", configs["my_name"])
+  
+  for mail in mails:
+      send_mail(configs["my_mail"], mail, subject_field.get(),  configs["password"], text)
+
+
+
+okno.title("DefSender")
+okno.geometry("720x480")
+
+
+
+
+subject_field = customtkinter.CTkEntry(okno, placeholder_text="")
+subject_field.insert(0, "") 
+subject_field.place(x=268, y=200)
+
+subject_field_label= customtkinter.CTkLabel(okno, text="Введите тему ->", fg_color="transparent")
+subject_field_label.place(x=163, y=200)
+
+
+
+
+
+
+friend_name = customtkinter.CTkEntry(okno, placeholder_text="")
+friend_name.insert(0, "") 
+friend_name.place(x=268, y=80)
+
+recipient_field_label = customtkinter.CTkLabel(okno, text=
+                                              '''Введите имя получателя ->
+(Необязательно)
+                                               ''', fg_color="transparent")
+recipient_field_label.place(x=90, y=80)
+
+
+
+recipient_field_label = customtkinter.CTkLabel(okno, text="Введите ваше имя ->", fg_color="transparent")
+recipient_field_label.place(x=130, y=40)
+
+copyright = customtkinter.CTkLabel(okno, text="Все права защищены Defsender 2024 ©", fg_color="transparent", font=('Times 30',11) )
+copyright.place(x=10, y=450)
+
+version = customtkinter.CTkLabel(okno, text="Beta 2.1", fg_color="transparent", font=('Times 30',11) )
+version.place(x=675, y=450)
+
+support = customtkinter.CTkLabel(okno, text="Написать в поддержку - defsender@yandex.ru", fg_color="transparent", font=('Times 30',13) )
+support.place(x=220, y=290)
+
+
+
+
+
+
+def checkbox_event():
+    print("Cогласие:", check_var.get())
+
+check_var = customtkinter.StringVar(value="Отклонено")
+checkbox = customtkinter.CTkCheckBox(okno, text="Данные указаны верно", command=checkbox_event,
+                                     variable=check_var, onvalue="Принято", offvalue="Отклонено")
+checkbox.place(x=250, y=330)
+
+
+button = customtkinter.CTkButton(master=okno, text="Начать рассылку", command=start)
+button.place(relx=0.47, rely=0.8, anchor=customtkinter.CENTER)
+
+
+
+button_image = customtkinter.CTkImage(Image.open("./cogwheel.png"), size=(30, 30))
+button = customtkinter.CTkButton(master=okno, image = button_image, fg_color="transparent", text="", width=35, height=35, command=config)
+button.place(x=690 ,y=410, anchor=customtkinter.CENTER)
+
+
+logo = customtkinter.CTkLabel(okno, text="DefSender", fg_color="transparent", font=('Times 30',35))
+logo.place(x=500, y=50)
+logo_description = customtkinter.CTkLabel(okno, text="Приложение для рассылки", fg_color="transparent", font=('Times 30',15))
+logo_description.place(x=490, y=90)
+
+# test = ttk.Label(text = "rujtghyuiwrjghujwr")
+# test.place(x=320, y=300)
+
+
+
+
+
+
+okno.mainloop()
---
+++ settings.py
@@ -0,0 +1,107 @@
+import customtkinter
+import json
+
+
+
+
+
+def config():
+    setting =  customtkinter.CTk()
+
+    setting.title("Настройки")
+
+    setting.geometry("450x450") 
+
+
+    with open("config.json", "r", encoding="UTF-8") as config_js:
+        configs = config_js.read()
+    configs = json.loads(configs)
+
+
+    customtkinter.set_appearance_mode("dark") 
+
+
+
+    my_name_entry = customtkinter.CTkEntry(setting, placeholder_text=configs["my_name"])
+    my_name_entry.place(x=161,y=150)
+
+
+    my_mail_entry = customtkinter.CTkEntry(setting, placeholder_text=configs["my_mail"])
+    my_mail_entry.place(x=161,y=200)
+
+
+    password_entry = customtkinter.CTkEntry(setting, placeholder_text=configs["password"])
+    password_entry.place(x=161,y=250)
+
+    main_label = customtkinter.CTkLabel(setting, text="Настройки", fg_color="transparent", font=('Times 30',35) )
+    main_label.place(x=140, y=70)
+
+    my_mail_label = customtkinter.CTkLabel(setting, text="Ваша почта", fg_color="transparent", font=('Times 30',11) )
+    my_mail_label.place(x=95, y=150)
+
+    password_label = customtkinter.CTkLabel(setting, text="Пароль от почты", fg_color="transparent", font=('Times 30',11) )
+    password_label.place(x=65, y=200)
+
+    my_name_label = customtkinter.CTkLabel(setting, text="Ваше имя", fg_color="transparent", font=('Times 30',11) )
+    my_name_label.place(x=100, y=250)
+
+
+
+
+
+
+
+
+    
+    button = customtkinter.CTkButton(setting, text="Сохранить настройки", command=lambda: save_settings(my_mail_entry, password_entry ,my_name_entry, configs, setting))
+    button.place(x=161,y=350)
+    setting.mainloop()
+
+
+
+
+def save_settings(my_mail_entry, password_entry ,my_name_entry,configs,setting):
+
+    new_settings = {}
+    #проверка маймейл
+    if my_mail_entry.get() == "":
+        new_settings["my_mail"] = configs["my_mail"]
+    else:
+        new_settings["my_mail"] = my_mail_entry.get()
+
+    #проверка пассворд
+    if password_entry.get() == "":
+        new_settings["password"] = configs["password"]
+    else:
+        new_settings["password"] = password_entry.get()
+
+    #проверка майнейма
+    if my_name_entry.get() == "":
+        new_settings["my_name"] = configs["my_name"]
+    else:
+        new_settings["my_name"] = my_name_entry.get()
+
+
+    # new_settings= {
+    #     "my_mail" : my_mail_entry.get(),
+    #     "password": password_entry.get(),
+    #     "my_name" : my_name_entry.get()
+    # }
+
+    print(new_settings)
+    
+
+
+    with open('config.json', 'w', encoding='UTF-8') as file:
+        json.dump(new_settings, file)
+    
+    setting.destroy()
+    
+    
+    
+
+
+
+
+
+   
